29507ec28078e07ada7803773bef55511bc80ccf
Wikaobl <wikob@icloud.com>
2023-10-17 12:14:12 +0200

    changing js file

--- index.js
+++ index.js
@@ -1,59 +1,36 @@
-const users = new Map([
-  ["x", "12345678"],
-  ["y", "11223344"],
-  ["test", "00000000"],
+const votingForm = document.querySelector(".voting-form");
+const voteOptions = document.querySelector(".vote-options");
+const userName = document.querySelector(".user-name");
+const userID = document.querySelector(".user-id");
+const resultDiv = document.querySelector(".vote-results");
+
+const validUserIds = new Map([
+  ["Romuś", "12345678"],
+  ["Stasio", "11111111"],
+  ["Barti", "12341234"],
+  ["Dżastina", "00000001"],
 ]);
 
-const votes = new Map();
+const voteCounts = new Map();
+voteCounts.set("Opcja 1", 0);
+voteCounts.set("Opcja 2", 0);
+voteCounts.set("Opcja 3", 0);
 
-votes.set("opcja 1", 0);
-votes.set("opcja 2", 0);
-votes.set("opcja 3", 0);
-votes.set("opcja 4", 0);
+const votedUsers = new Set();
 
-const usersAlreadyVoted = new Set();
-
-const voteForm = document.querySelector(".vote-form"); // formularz
-const voteOptions = document.querySelector(".vote-options"); // opcje
-const userName = document.querySelector(".user-name"); // input dla nazwy użytkownika
-const userID = document.querySelector(".user-id"); // input dla identyfikatora użytkownika
-const resultsDiv = document.querySelector(".vote-results"); // div gdzie będą wyniki głosowania
-
-const updateResults = () => {
-  let output = "";
-
-  for (const [option, count] of votes) {
-    output += `${option} : ${count}  głosów<br>`; // Dla każdej pary klucz-wartość budujemy <p> z treścią
-  }
-  resultsDiv.innerHTML = output;
-};
-updateResults();
-
-const getVote = (e) => {
+const voting = (e) => {
   e.preventDefault();
-
-  const user = userName.value;
-  const password = userID.value;
-  const answer = voteOptions.value;
-
-  if (users.has(user)) {
-    if (users.get(user) !== password) {
-      alert("Nieprawidłowe hasło.");
-    }
-
-    if (usersAlreadyVoted.has(user)) {
-      alert("Już zagłosowałeś. Dziękujemy.");
-    } else {
-      usersAlreadyVoted.add(user);
-      console.log(usersAlreadyVoted);
-
-      votes.set(answer, votes.get(answer) + 1);
-    }
-  } else {
-    alert("Nieprawidłowe dane.");
-  }
+  //Logikę
 
   updateResults();
 };
 
-voteForm.addEventListener("submit", getVote);
+const updateResults = () => {
+  let output = "";
+  for (const [option, count] of voteCounts) {
+    output += `${option} : ${count} głosów.`;
+  }
+  resultDiv.innerHTML = output;
+};
+
+votingForm.addEventListener("submit", voting);
